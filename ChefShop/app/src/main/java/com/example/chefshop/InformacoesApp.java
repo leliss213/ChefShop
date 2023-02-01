09aae0d9201e6f13e4e5214ad0d2e02addd58e36
alicinhaf <alicehfengler@gmail.com>
2023-02-01 09:07:05 -0300

    final do recycler

--- ChefShop/app/src/main/java/com/example/chefshop/InformacoesApp.java
+++ ChefShop/app/src/main/java/com/example/chefshop/InformacoesApp.java
@@ -5,16 +5,26 @@ import android.app.Application;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
+import modelDominio.Receita;
 import modelDominio.Usuario;
 
 public class InformacoesApp extends Application {
     public Socket socket;
     public ObjectOutputStream out;
     public ObjectInputStream in;
+    private ArrayList<Receita> listaReceitas;
 
     private Usuario usuarioLogado;
     private Usuario usuarioInserido;
+
+    //onCreate
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listaReceitas = new ArrayList<>();
+    }
 
     //getters
     public Usuario getUsuarioLogado() {
@@ -23,18 +33,18 @@ public class InformacoesApp extends Application {
     public Usuario getUsuarioInserido() {
         return usuarioInserido;
     }
+    public ArrayList<Receita> getListaReceitas() {
+        return listaReceitas;
+    }
 
     //setters
     public void setUsuarioLogado(Usuario usuarioLogado) {
         this.usuarioLogado = usuarioLogado;
     }
-
     public void setUsuarioInserido(Usuario usuarioInserido) {
         this.usuarioInserido = usuarioInserido;
     }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    public void setListaReceitas(ArrayList<Receita> listaReceitas) {
+        this.listaReceitas = listaReceitas;
     }
 }
--- ChefShop/app/src/main/java/com/example/chefshop/VisualizacaoReceitasRecyclerActivity.java
+++ ChefShop/app/src/main/java/com/example/chefshop/VisualizacaoReceitasRecyclerActivity.java
@@ -1,5 +1,6 @@
 package com.example.chefshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,9 +8,13 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+
+import modelDominio.Receita;
 
 public class VisualizacaoReceitasRecyclerActivity extends AppCompatActivity {
     RecyclerView rvElementos;
@@ -25,12 +30,33 @@ public class VisualizacaoReceitasRecyclerActivity extends AppCompatActivity {
 
         rvElementos = (RecyclerView) findViewById(R.id.rvElementos);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         //obtendo o contexto
         informacoesApp = (InformacoesApp) getApplicationContext();
 
-
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (informacoesApp.getListaReceitas() != null) {
+            receitaAdapter = new ReceitaAdapter(informacoesApp.getListaReceitas(), trataCliqueItem); //criei o adapter;
+            rvElementos.setLayoutManager(new LinearLayoutManager(VisualizacaoReceitasRecyclerActivity.this));
+            rvElementos.setItemAnimator(new DefaultItemAnimator());
+            rvElementos.setAdapter(receitaAdapter);
+        }
     }
+
+    //fora do onCreate mas dentro da activity eu vou criar o trata clique:
+    //listener que vai tratar o clique nos elementos do recycler
+    ReceitaAdapter.ReceitaOnClickListener trataCliqueItem = new ReceitaAdapter.ReceitaOnClickListener() {
+        @Override
+        public void onClickReceita(View view, int position) {
+            //Obtendo o curso do clique:
+            Receita minhaReceita = informacoesApp.getListaReceitas().get(position);
+            //Chamar a tela -> criar a intent:
+//            Intent it = new Intent(VisualizacaoReceitasRecyclerActivity.this,VisualizacaoDetalhadaActivity.class);
+            //Adicionando o curso na solicitação:
+//            it.putExtra("curso",meuCurso);
+            //Enviando a solicitação:
+//            startActivity(it);
+        }
+    };
 
 }
