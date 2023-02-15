7f65ea929d2108d73a7e41f568ec30527315ffb2
alicinhaf <123584424+alicinhaf@users.noreply.github.com>
2023-02-15 11:50:25 -0300

    agroa

--- ChefShopServidor/src/model/ReceitaDao.java
+++ ChefShopServidor/src/model/ReceitaDao.java
@@ -51,4 +51,45 @@ public class ReceitaDao {
         }
     }
         
+//    public int inserir(Receita receita) {
+//        //vai receber o script SQL de INSERT 
+//        PreparedStatement stmt = null;
+//        try {
+//            try {
+//                // desliga o autocommit
+//                con.setAutoCommit(false);
+//                String sql = "insert into bike (modelo,codmarca,preco,imagem,datalancamento) values (?,?,?,?,?)";
+//                stmt = con.prepareStatement(sql);
+//                //substituir os ? do script SQL
+//                stmt.setString(1, bk.getModelo());
+//                stmt.setInt(2, bk.getMarca().getCodmarca());
+//                stmt.setDouble(3, bk.getPreco());
+//                stmt.setBytes(4, bk.getImagem());
+//                stmt.setDate(5, new java.sql.Date(bk.getDataLancamento().getTime()));
+//                //executar o SCRIPT SQL
+//                stmt.execute();
+//                //efetivar a transação
+//                con.commit();
+//                return -1; // <- indica que tudo deu CERTO
+//            } catch (SQLException e) {
+//                try {
+//                    con.rollback(); // cancelando a transação 
+//                    e.printStackTrace();
+//                    return e.getErrorCode(); // devolvendo o erro
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                    return ex.getErrorCode();
+//                }
+//            }
+//        } finally {// isto será executado dando ERRO ou NÃO
+//            try {
+//                stmt.close();
+//                con.setAutoCommit(true);
+//                con.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//                return e.getErrorCode();
+//            }
+//        }
 }
+    
