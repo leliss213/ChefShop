23d8e1b4475ba0c816d3cf549013270f09bb2069
ailavsilva <ailaaabsilva@hotmail.com>
2023-02-22 10:51:17 -0300

    mini correções pq o resto nao deu certo

---
+++ ChefShopAdministrador/src/view/tablemodel/ReceitaTableModel.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view.tablemodel;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import modelDominio.Receita;
+/**
+ *
+ * @author Aila e Alice
+ */
+public class ReceitaTableModel extends AbstractTableModel{
+
+    private ArrayList<Receita> listaReceitas;
+
+    public ReceitaTableModel(ArrayList<Receita> listaReceitas) {
+        this.listaReceitas = listaReceitas;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return listaReceitas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Receita rc = listaReceitas.get(rowIndex);
+        
+        switch (columnIndex) {
+            case 0:  return rc.getNomeReceita();
+            case 1:  return rc.getTipo();
+            case 2:  return rc.getIngredientes();
+            default: return "";
+        }
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:  return "Nome da Receita";
+            case 1:  return "Categoria";
+            case 2:  return "Ingredientes";
+            default: return "NoName";
+        }
+    }
+    
+    public Receita getReceita(int row){
+        return listaReceitas.get(row);
+    }
+    
+}
